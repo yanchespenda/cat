e13e0865d33a2e90431d51334a0b64385bf7c1d9
Nur Akhwan <akhwan90@gmail.com>
2017-01-27 09:02:39 +0700

    Update db_cat_dua.sql

--- db_cat_dua.sql
+++ db_cat_dua.sql
@@ -1,16 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versi server:                 5.6.25 - MySQL Community Server (GPL)
--- OS Server:                    Win32
--- HeidiSQL Versi:               9.4.0.5125
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- membuang struktur untuk table db_cat_dua.m_admin
 CREATE TABLE IF NOT EXISTS `m_admin` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
@@ -240,6 +227,3 @@ END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
