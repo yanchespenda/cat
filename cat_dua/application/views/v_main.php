f0b934108cfea0ea6306ccaf28e93be64883dae1
Nur Akhwan <akhwan90@gmail.com>
2017-02-27 21:17:56 +0700

    Update v_main.php

--- cat_dua/application/views/v_main.php
+++ cat_dua/application/views/v_main.php
@@ -1,4 +1,4 @@
-<div class="row col-md-12">
+<div class="row col-md-12 ini_bodi">
   <div class="panel panel-info">
     <div class="panel-heading">Selamat datang di Sistem Ujian Online</div>
     <div class="panel-body">
