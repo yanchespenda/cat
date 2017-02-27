efa5837b79b0799f890d338ac80f982c958aa2f8
Nur Akhwan <akhwan90@gmail.com>
2017-02-27 21:16:37 +0700

    Update m_list_ujian_siswa.php

--- cat_dua/application/views/m_list_ujian_siswa.php
+++ cat_dua/application/views/m_list_ujian_siswa.php
@@ -1,4 +1,4 @@
-<div class="row col-md-12">
+<div class="row col-md-12 ini_bodi">
   <div class="panel panel-info">
     <div class="panel-heading">Daftar Ujian / Tes</div>
     <div class="panel-body">
