e72bc8e95e530418c9db82803a6062fb4342ea0b
Nur Akhwan <akhwan90@gmail.com>
2018-01-12 10:29:47 +0700

    update checking waktu ujian

--- cat_dua/application/views/m_token.php
+++ cat_dua/application/views/m_token.php
@@ -5,6 +5,7 @@
     <div class="panel-body">
       <input type="hidden" name="id_ujian" id="id_ujian" value="<?php echo $du['id']; ?>">
       <input type="hidden" name="_token" id="_token" value="<?php echo $du['token']; ?>">
+      <input type="hidden" name="_tgl_sekarang" id="_tgl_sekarang" value="<?php echo date('Y-m-d H:i:s'); ?>">
       <input type="hidden" name="_tgl_mulai" id="_tgl_mulai" value="<?php echo $tgl_mulai; ?>">
       <input type="hidden" name="_terlambat" id="_terlambat" value="<?php echo $terlambat; ?>">
       <div class="col-md-7">
@@ -30,9 +31,18 @@
               Waktu boleh mengerjakan ujian adalah saat tombol "MULAI" berwarna hijau..!
             </div>
 
-            <div class="btn btn-info btn-lg" id="btn_mulai"></div>
+            <div id="btn_mulai"></div>
+
+            <div class="btn btn-danger" id="waktu_" style="margin-top: 20px">
+              Sisa waktu mengikuti ujian : <span id="waktu_akhir_ujian"></span>
+            </div>
+
+            <div id="waktu_game_over"></div>
+
+            <!--
             <a href="#" class="btn btn-success btn-lg" id="tbl_mulai" onclick="return konfirmasi_token(<?php echo $du['id']; ?>)"><i class="fa fa-check-circle"></i> MULAI</a>
             <div class="btn btn-danger" id="ujian_selesai">UJIAN TELAH SELESAI</div>
+            -->
           </div>
         </div>
       </div>
